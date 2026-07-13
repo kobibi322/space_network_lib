d75b3b4e9a911986d3b0264937afd7f43318bacf
Yakov B <kobibiton33@gmail.com>
2026-07-13 15:54:09 +0300

    שלב 1

--- space_network_lib.py
+++ space_network_lib.py
@@ -91,13 +91,3 @@ class SpaceNetwork:
             f"[Network] Transmitting from {source_entity.name} to {dest_entity.name}..."
         )
         dest_entity.receive_signal(packet)
-
-class Satellite(SpaceEntity):
-    def receive_signal(self, packet: Packet):
-        print(f"[{self.name}] Received packet: {packet}")
-
-space_network_1 = SpaceNetwork(level=1)
-satellite_1 = Satellite(name="Satellite 1", distance_from_earth=100)
-satellite_2 = Satellite(name="Satellite 2", distance_from_earth=200)
-packet_1 = Packet(data="Hello, Satellite 2!", sender=satellite_1, receiver=satellite_2)
-space_network_1.send(packet_1)
